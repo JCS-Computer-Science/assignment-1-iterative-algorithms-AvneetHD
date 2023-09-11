c5c29a3884dab8cfb0af499f0a3f9b6781e62fbe
Avneet Hayer <16139005@edu.cbe.ab.ca>
2023-09-11 10:20:32 -0600

    work on selection sort

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,5 +1,34 @@
 function bubbleSort(array) {
-	/*
+	let finalArray_theSecond = array.length;
+    let sorted = false;
+
+    while(!sorted){
+        if(finalArray_theSecond > 0){
+            for(let i=1; i <array.length; i++){
+                if(array[i] >= array[i-1]){
+                    console.log("sorted");
+                    sorted = true
+                }else if(array[i] < array[i-1]){
+                    console.log("unsorted");
+                    var x = array[i];
+                    var y = array[i-1];
+                    array[i-1] = x;
+                    array[i] = y;
+                    sorted = false
+                    break;
+                }
+            }
+        }else{
+            sorted = true;
+        }
+    }
+
+	return array;
+}
+module.exports = bubbleSort;
+
+
+/*
     ** Compare the first two elements and swap if necessary.
     ** Then compare the second and third elements and swap if
     ** necessary. Continue until you've checked and swapped (if needed)
@@ -9,7 +38,4 @@ function bubbleSort(array) {
     ** cycle without needing any swaps
     */
 
-	return array;
-}
-
-module.exports = bubbleSort;
+console.log(bubbleSort([1, 2, 3, 4, 6, 7, 8, 9, 10, 14, 21, 32, 33, 34, 4000]));
--- selection-sort.js
+++ selection-sort.js
@@ -6,8 +6,19 @@ function selectionSort(array) {
 	 ** for the smallest, then swap it with the second element.
 	 ** Continue like this until you have only one element left.
 	 */
+	let smallest = array[0];
+	for(let i=0; i < array.length; i++){
+		var x = array[i];
+		if( x < smallest){
+			smallest = x;
+		}
+
+	}
+	console.log(smallest);
 
 	return array;
 }
 
 module.exports = selectionSort;
+
+selectionSort([3, 5, 420, 69, 1]);
