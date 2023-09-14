67d08e35529eeba395109fa516b1db0571a89d7c
Avneet Hayer <16139005@edu.cbe.ab.ca>
2023-09-14 11:29:59 -0600

    I did it mom

--- binary-search.js
+++ binary-search.js
@@ -1,13 +1,32 @@
 function binarySearch(array, searchTerm) {
-	let x = Math.floor(array.length / 2);
-	
+	let sorted = false;
+	let start = 0;
+	let end = array.length - 1;
+	let arrayHalved = Math.floor((start + end) / 2);
+	let counter = 0;
 
-	return null;
+	while(!sorted && counter < 100){
+		arrayHalved = Math.round((start + end) / 2);
+		console.log(arrayHalved);
+
+		if(array[arrayHalved] == searchTerm){
+			console.log("searchterm is at index: "+ arrayHalved);
+			sorted = true;
+		}else if(array[arrayHalved] < searchTerm){
+			console.log("look at top half");
+			start = arrayHalved + 1;
+		}else if(array[arrayHalved] > searchTerm){
+			console.log("look at bottom half");
+			end = arrayHalved - 1;
+		}
+		counter++;
+	}
+	return arrayHalved;
 }
 
 module.exports = binarySearch;
 
-
+binarySearch([1], 1)
 	/*
 	 ** Check the middle element, if it's higher than the searchTerm
 	 ** then check the element in between the start and the middle, otherwise
